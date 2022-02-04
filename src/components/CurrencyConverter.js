114744b54a2ba768994687c93910bdc9403ee5a5
Hans Bastiaan <h.bastiaan@windesheim.nl>
2022-02-04 13:59:50 +0100

    CurrencyConverter

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,9 +1,74 @@
 import ExchangeRate from "./ExchangeRate";
 
 const CurrencyConverter = () => {
+    const currencies = ["BTC", "ETH", "XRP", "LTC", "ADA", "EUR", "USD"];
+
+    // BTC = Bitcoins
+    // ETH = Ethereum
+    // XRP = XRP
+    // LTC = Lattice Token
+    // ADA = Cardano
+    // EUR = EURO
+
     return (
         <div className="currency-converter">
-            <p>Welcome to the currency converter</p>
+            <h2>Currency Converter</h2>
+            <div className="input-box">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Primary Currency</td>
+                            <td>
+                                <input
+                                    type="number"
+                                    name="primaryCurrency"
+                                    value={""}
+                                />
+                            </td>
+                            <td>
+                                <select
+                                    value={""}
+                                    name="primaryCurrencyOption"
+                                    className="currency-options"
+                                >
+                                    {currencies.map(
+                                        (currency: string, _index: number) => (
+                                            <option key={_index}>
+                                                {currency}
+                                            </option>
+                                        )
+                                    )}
+                                    ;
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Secundary Currency</td>
+                            <td>
+                                <input
+                                    type="number"
+                                    name="secondaryCurrency"
+                                    value={""}
+                                />
+                            </td>
+                            <td>
+                                <select
+                                    value={""}
+                                    name="secondaryCurrencyOption"
+                                    className="currency-options"
+                                >
+                                    {currencies.map(
+                                        (currency: string, _index: number) => (
+                                            <option>{currency}</option>
+                                        )
+                                    )}
+                                    ;
+                                </select>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             <ExchangeRate />
         </div>
     );
